428397df6e1e8f03779bacdc3011b7c11643c8ea
josha Muinde <muindejoshua8@gmail.com>
2023-09-08 14:27:46 +0300

    Image folder and files added

--- index.php
+++ index.php
@@ -7,6 +7,44 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header>
+        <h1>Mario Club</h1>
+    </header>
+    <section class="banner">
+        <img src="img/banner.png" alt="Mario Club welcome banner">
+        <div class="welcome">
+            <h2>Welcome to <br><span>Mario Club</span></h2>
+        </div>
+    </section>
+    <nav class="main-nav">
+        <ul>
+            <li><a href="/join.php" class="join">Join the club</a></li>
+            <li><a href="/news.php">Latest News</a></li>
+            <li><a href="/games.php">New games</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <article>
+            <h2>It's a me, Mario</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam blanditiis tempore ad, earum eum officia eaque aut ea quos nihil assumenda odit voluptate perspiciatis. Fugiat vitae sapiente laudantium ipsum voluptate? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ipsum natus voluptates placeat odio earum nostrum libero iure a doloremque ex praesentium ab culpa voluptatibus officiis assumenda labore, nulla illo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius perferendis dolorum, dolore dolor corrupti fuga aut! Perferendis, cumque, nulla debitis necessitatibus omnis vitae magnam cum temporibus iusto non, sapiente molestiae?</p>
+        </article>
+        <ul class="">
+            <li><img src="img/thumb-1.png" alt="Mario Thumb 1"></li>
+            <li><img src="img/thumb-2.png" alt="Mario Thumb 2"></li>
+        </ul>
+        <section class="join">
+            <h2>Join Today</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora dignissimos molestiae architecto explicabo iusto obcaecati ex magni repudiandae assumenda nostrum id</p>
+            <form>
+                <input type="email" name="email" placeholder="Type email here" required>
+            </form>
+        </section>
+        <footer>
+            <p class="copyright">
+                copyright 2023 MarioClub
+            </p>
+        </footer>
+    </main>
 </body>
 </html>
